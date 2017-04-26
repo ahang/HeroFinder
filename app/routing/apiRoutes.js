afcc104ea174640afb402d192cb47c4ea2c4e654
Andy Hang <hang.andy.t@gmail.com>
2017-04-26 14:11:48 -0500

    small changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,45 +18,30 @@ module.exports = function(app) {
 
         console.log(request.body);
         var userArray = request.body.scores;
-        var userInt = [];
-
-        for (var i = 0; i < userArray.length; i++) {
-            userInt.push(userArray[i]);
-            console.log(userInt);
+        console.log(`Before convert ${userArray}`);
+        function convertInt(arr) {
+            arr = arr.map(function(n) {
+                return parseInt(n);
+            });
         }
-        //console.log(userArray);
-        //console.log(friendData);
-        // for (var i = 0; i < friendData.length; i++) {
-        //     console.log(`${friendData[i].name} || ${friendData[i].scores}`);
-        // }
+        convertInt(userArray);
+        console.log(`After convert ${userArray}`);
 
-        // function compare() {
-        //     for (var i = 0; i < friendData.length; i++) {
-        //         if (friendData[i].scores === parseInt(friendData[i].scores, 10)) {
-        //             console.log(`${friendData[i].name} contains an int`);
-        //         } else {
-        //             console.log(`${friendData[i].name} does not contain an int`)
-        //         }
+        // function distance(arr1, arr2) {
+        //     for (var i = 0; i < arr1.length; i ++) {
+        //         console.log(`This is values before .map ${arr1}`);
+        //         //console.log(`This is subtracting before .map ${arr1.scores[i] - arr2.scores[i]}`);
+
+        //         arr1 = arr1.map(function(n) {
+        //             return parseInt(n);
+        //         });
+
+        //         console.log(`This is values after .map ${arr1}`);
+        //         console.log(`This is subtracting after .map ${arr1.scores[i] - arr2.scores[i]}`);
+        //         //var distance = parseInt(arr1[i] - arr2[i])
+        //         //console.log(`The ${[i]} distance is ${distance}`);
         //     }
         // }
-
-        difference(userArray, friendData[1].scores);
-
-        function difference(arr1, arr2) {
-            for (var i = 0; i < arr1.length; i ++) {
-                console.log(`This is values before .map ${arr1}`);
-                //console.log(`This is subtracting before .map ${arr1.scores[i] - arr2.scores[i]}`);
-
-                arr1 = arr1.map(function(n) {
-                    return parseInt(n);
-                });
-
-                console.log(`This is values after .map ${arr1}`);
-                console.log(`This is subtracting after .map ${arr1.scores[i] - arr2.scores[i]}`);
-                //var distance = parseInt(arr1[i] - arr2[i])
-                //console.log(`The ${[i]} distance is ${distance}`);
-            }
-        }
         response.send("HELP");
     });
 };
